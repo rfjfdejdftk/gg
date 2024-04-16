6df132ce629d14f7f180901ab7c8d8163b60d431
rfjfdejdftk <164313907+rfjfdejdftk@users.noreply.github.com>
2024-04-16 10:24:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,189 @@
+import {
+    watchFile,
+    unwatchFile
+} from 'fs';
+import chalk from 'chalk';
+import {
+    fileURLToPath
+} from 'url';
+import fs from 'fs';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.owner = [
+    ['201128698632', '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿', true],
+    ['201069799352', '🔥 𝐀𝐍𝐆𝐄𝐋𝐎? 🧿', true],
+];
+
+global.xaxa = 'HU TAO';
+global.suittag = ['201128698632', '201069799352'];
+global.prems = ['201128698632', '201069799352'];
+
+(function (_0x241ce1, _0xdf71e6) {
+    var _0x43c878 = _0x241ce1();
+    while (!![]) {
+        try {
+            var _0x215712 = parseInt(_0x2b18(0x4)) / 0x1 + -parseInt(_0x2b18(0x0)) / 0x2 + -parseInt(_0x2b18(0x7)) / 0x3 + parseInt(_0x2b18(0x2)) / 0x4 + parseInt(_0x2b18(0x5)) / 0x5 * (parseInt(_0x2b18(0x8)) / 0x6) + -parseInt(_0x2b18(0x3)) / 0x7 * (parseInt(_0x2b18(0x1)) / 0x8) + parseInt(_0x2b18(0x9)) / 0x9;
+            if (_0x215712 === _0xdf71e6) {
+                break;
+            } else {
+                _0x43c878['push'](_0x43c878['shift']());
+            }
+        } catch (_0x1a2946) {
+            _0x43c878['push'](_0x43c878['shift']());
+        }
+    }
+}(_0x2e29, 0x79963));
+global['anabro'] = '乂 ❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 乂';
+global[_0x2b18(0x6)] = ' 🧿 𝐀𝐍𝐆𝐄𝐋𝐎 ❄️\x0aꨁ +201128698632 ꨁ\x0a🧿 𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓🔥\x0aꨁ +201069799352 ꨁ';
+
+function _0x2b18(_0x270df, _0x2e29b1) {
+    var _0x2b1884 = _0x2e29();
+    _0x2b18 = function (_0x3679ec, _0x1e40bc) {
+        _0x3679ec = _0x3679ec - 0x0;
+        var _0x439607 = _0x2b1884[_0x3679ec];
+        return _0x439607;
+    };
+    return _0x2b18(_0x270df, _0x2e29b1);
+}
+global['veeee'] = '乂 ❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 乂';
+
+function _0x2e29() {
+    var _0x1fd07a = ['560945yKSVLB', '32105dLquCN', 'veeeee', '2391705XyocYx', '900ljCNFK', '6737922uZCWCv', '1813774LnenCF', '464spUwhW', '2291128LpLmmw', '77651ZapIyZ'];
+    _0x2e29 = function () {
+        return _0x1fd07a;
+    };
+    return _0x2e29();
+}
+global.packname = '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 ';
+global.author = '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿';
+global.wm = '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿';
+global.titulowm = '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿';
+global.titulowm2 = `乂 ❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 乂`
+global.igfg = '❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿';
+global.wait = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
+
+global.mods = [];
+
+global.d = new Date(new Date + 3600000);
+global.locale = 'ar';
+global.dia = d.toLocaleDateString(locale, {
+    weekday: 'long'
+});
+global.fecha = d.toLocaleDateString('ar', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric'
+});
+global.mes = d.toLocaleDateString('ar', {
+    month: 'long'
+});
+global.año = d.toLocaleDateString('ar', {
+    year: 'numeric'
+});
+global.tiempo = d.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+});
+
+global.wm2 = `▸ ${dia} ${fecha}\n▸ `;
+global.gt = '乂 ❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 乂';
+global.mysticbot = '乂 ❄️ 𝐀𝐍𝐆𝐄𝐋𝐎 🧿 乂';
+(function (_0x2c1fa5, _0x22b6e8) {
+    var _0x42866e = _0x2c1fa5();
+    while (!![]) {
+        try {
+            var _0x4fb938 = -parseInt(_0x165a(0x2)) / 0x1 + parseInt(_0x165a(0xa)) / 0x2 * (parseInt(_0x165a(0x1)) / 0x3) + parseInt(_0x165a(0x0)) / 0x4 * (parseInt(_0x165a(0x5)) / 0x5) + -parseInt(_0x165a(0x6)) / 0x6 * (parseInt(_0x165a(0x8)) / 0x7) + -parseInt(_0x165a(0x9)) / 0x8 + parseInt(_0x165a(0x7)) / 0x9 * (parseInt(_0x165a(0x4)) / 0xa) + -parseInt(_0x165a(0xb)) / 0xb * (-parseInt(_0x165a(0x3)) / 0xc);
+            if (_0x4fb938 === _0x22b6e8) {
+                break;
+            } else {
+                _0x42866e['push'](_0x42866e['shift']());
+            }
+        } catch (_0x591efe) {
+            _0x42866e['push'](_0x42866e['shift']());
+        }
+    }
+}(_0x4b4a, 0x711a6));
+
+function _0x4b4a() {
+    var _0x3eb071 = ['3169257BDLHGN', '4516176ZScEpL', '61442lvptdX', '2992WdidlR', '24956aynNVT', '15MiPXTl', '773659gxUHlr', '61092BCIStB', '114830zopwBa', '270Jdvxlh', '6GnIuzS', '297WtaMRl'];
+    _0x4b4a = function () {
+        return _0x3eb071;
+    };
+    return _0x4b4a();
+}
+
+function _0x165a(_0x126caf, _0x4b4a8d) {
+    var _0x165a73 = _0x4b4a();
+    _0x165a = function (_0x18a1a8, _0x1b73c3) {
+        _0x18a1a8 = _0x18a1a8 - 0x0;
+        var _0x541f50 = _0x165a73[_0x18a1a8];
+        return _0x541f50;
+    };
+    return _0x165a(_0x126caf, _0x4b4a8d);
+}
+global['md'] = 'https://chat.whatsapp.com/HzfsFIfYTj7HdjbekZPfIT';
+global['mysticbot'] = 'https://chat.whatsapp.com/HzfsFIfYTj7HdjbekZPfIT';
+global.waitt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+global.waittt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+global.waitttt = '*⌛ _جاري التحميل..._*\n\n*▰▰▰▱▱▱▱▱*';
+global.nomorown = '20112869832';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}`; // Asia/Jakarta
+global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`; // America/Los_Angeles
+global.fgif = {
+    key: {
+        participant: '0@s.whatsapp.net'
+    },
+    message: {
+        'videoMessage': {
+            'title': wm,
+            'h': `Hmm`,
+            'seconds': '999999999',
+            'gifPlayback': 'true',
+            'caption': bottime,
+            'jpegThumbnail': fs.readFileSync('./Menu.png')
+        }
+    }
+};
+global.multiplier = 99;
+global.flaaa = [
+    'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+    'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+    'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+    'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+    'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+
+
+const file = fileURLToPath(
+    import.meta.url);
+watchFile(file, () => {
+    unwatchFile(file);
+    console.log(chalk.redBright('Update \'config.js\''));
+    import(`${file}?update=${Date.now()}`);
+});
